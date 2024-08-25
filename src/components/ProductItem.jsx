15cb6fbabb01389c9cdbf54f965d8bf20c1ecde5
ghmamadreza2 <ghmamadreza84@gmail.com>
2024-08-25 23:31:36 +0430

    i get currency from shopContext

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -1,12 +1,10 @@
-import React from "react";
+import React, { useContext } from "react";
+import { ShopContext } from "../context/ShopContext";
 
-const ProductItem = ({id,image,name,price}) => {
+const ProductItem = ({ id, image, name, price }) => {
+  const { currency } = useContext(ShopContext);
 
-  
-  return <div>
-
-
-  </div>;
+  return <div></div>;
 };
 
 export default ProductItem;
