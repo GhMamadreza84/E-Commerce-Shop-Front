26753f6ab666b813f3cb35b9b4c5ce36e5187834
ghmamadreza2 <ghmamadreza84@gmail.com>
2024-08-25 23:39:40 +0430

    i create a rendering product section in latestCollection component

--- src/components/LatestCollection.jsx
+++ src/components/LatestCollection.jsx
@@ -18,7 +18,8 @@ const LatestCollection = () => {
           fugit laudantium neque dolorem accusamus!
         </p>
       </div>
-      
+      {/* Rendering Products */}
+      <div className="grid grid-cols-2 gap-4 gap-y-6 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5"></div>
     </div>
   );
 };
