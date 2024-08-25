a0b4106baf8ba20ec66db141d722a64a369d9410
ghmamadreza2 <ghmamadreza84@gmail.com>
2024-08-25 23:32:30 +0430

    i use link for navigate user to move page product/id

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -1,10 +1,13 @@
 import React, { useContext } from "react";
 import { ShopContext } from "../context/ShopContext";
+import { Link } from "react-router-dom";
 
 const ProductItem = ({ id, image, name, price }) => {
   const { currency } = useContext(ShopContext);
 
-  return <div></div>;
+  return <Link to={`/product/${id}`}>
+    
+  </Link>;
 };
 
 export default ProductItem;
