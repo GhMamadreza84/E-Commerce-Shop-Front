639848db09385ac2a13fed7bdc06cc110727dcd6
ghmamadreza2 <ghmamadreza84@gmail.com>
2024-08-25 23:22:55 +0430

    i create new state for get and show 10 product in latest products

--- src/components/LatestCollection.jsx
+++ src/components/LatestCollection.jsx
@@ -1,15 +1,18 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { ShopContext } from "../context/ShopContext";
 import Title from "./Title";
 
 const LatestCollection = () => {
   const { products } = useContext(ShopContext);
+  const [latestProducts, setLatestProducts] = useState([]);
+
   return (
     <div className="my-10 ">
       <div className="py-8 text-3xl text-center">
         <Title text1={"LATEST"} text2={"COLLECTION"} />
         <p className="w-3/4 m-auto text-xs text-gray-600 sm:text-sm md:text-base">
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Autem ipsam, fugit laudantium neque dolorem accusamus!
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Autem ipsam,
+          fugit laudantium neque dolorem accusamus!
         </p>
       </div>
     </div>
