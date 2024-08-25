a2c78dc2388cc57d31a1a94c9d371a9bd3ea9913
ghmamadreza2 <ghmamadreza84@gmail.com>
2024-08-25 23:20:23 +0430

    i add p tag lorem in lastestcollection component

--- src/components/LatestCollection.jsx
+++ src/components/LatestCollection.jsx
@@ -3,12 +3,17 @@ import { ShopContext } from "../context/ShopContext";
 import Title from "./Title";
 
 const LatestCollection = () => {
-  const { products } = useContext(ShopContext); 
-  return <div className="my-10 ">
-    <div className="py-8 text-3xl text-center">
-      <Title text1={'LATEST'} text2={'COLLECTION'} />
+  const { products } = useContext(ShopContext);
+  return (
+    <div className="my-10 ">
+      <div className="py-8 text-3xl text-center">
+        <Title text1={"LATEST"} text2={"COLLECTION"} />
+        <p className="w-3/4 m-auto text-xs text-gray-600 sm:text-sm md:text-base">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Autem ipsam, fugit laudantium neque dolorem accusamus!
+        </p>
+      </div>
     </div>
-  </div>;
+  );
 };
 
 export default LatestCollection;
