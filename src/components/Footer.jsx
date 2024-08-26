ddc761bc5834291f940ad4853c8cab2d2a93e585
ghmamadreza2 <ghmamadreza84@gmail.com>
2024-08-26 23:23:43 +0430

    i create 2 part of form and i now go to resume for design another part

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
+import { assets } from '../assets/assets'
 
 const Footer = () => {
   return (
     <div>
-      
+      <div className="flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm">
+
+        <div className="">
+          <img className='w-32 mb-5' src={assets.logo} alt="logo" />
+          <p className='w-full text-gray-600 md:w-2/3 '>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem fuga cum ea ducimus est dolor.
+          </p>
+        </div>
+
+        <div className="">
+          <p className='mb-5 text-xl font-medium '>COMPANY</p>
+          <ul className='flex flex-col gap-1 text-gray-600 '>
+            <li>Home</li>
+            <li>About us</li>
+            <li>َDelivery</li>
+            <li>Privacy policy</li>
+          </ul>
+        </div>
+
+      </div>
     </div>
   )
 }
