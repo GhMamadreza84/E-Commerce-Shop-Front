1a08b1382425d4166c1a82f8fc212d0410260d07
ghmamadreza2 <ghmamadreza84@gmail.com>
2024-08-26 23:12:05 +0430

    i add class name for form tag and now newsletter component is finished

--- src/components/Newsletter.jsx
+++ src/components/Newsletter.jsx
@@ -7,7 +7,7 @@ const Newsletter = () => {
       <p className="mt-3 text-gray-400 ">
         Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quasi, soluta?
       </p>
-      <form >
+      <form className='flex items-center w-full gap-3 pl-3 mx-auto my-6 border sm:w-1/2'>
         <input className='flex w-full outline-none sm:flex-1 ' type="email" placeholder='Enter your email' required/>
         <button type='submit' className='px-10 py-4 text-xs text-white bg-black'>SUBSCRIBE</button>
       </form>
