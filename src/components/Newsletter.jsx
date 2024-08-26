91a829196532b65af2097c2f3c0a3b64d8c55a96
ghmamadreza2 <ghmamadreza84@gmail.com>
2024-08-26 22:48:21 +0430

    i add p tag in newsletter component and now need import in home and use it

--- src/components/Newsletter.jsx
+++ src/components/Newsletter.jsx
@@ -2,7 +2,8 @@ import React from 'react'
 
 const Newsletter = () => {
   return (
-    <div>
+    <div className='text-center'>
+      <p className='text-2xl font-medium text-gray-800'>Subscribe now & get 20% off</p>
       
     </div>
   )
