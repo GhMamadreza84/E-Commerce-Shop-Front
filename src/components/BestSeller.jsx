7b72eb6e3bc337472ba9959b9f8c013ca16cfa3e
ghmamadreza2 <ghmamadreza84@gmail.com>
2024-08-26 16:20:37 +0430

    i import title component in best seller for show title

--- src/components/BestSeller.jsx
+++ src/components/BestSeller.jsx
@@ -1,5 +1,6 @@
 import React, { useContext, useEffect } from "react";
 import { ShopContext } from "../context/ShopContext";
+import Title from "./Title";
 const BestSeller = () => {
   const { products } = useContext(ShopContext);
   const [bestSeller, setBestSeller] = useState([]);
@@ -9,7 +10,14 @@ const BestSeller = () => {
     setBestSeller(bestProducts.slice(0, 5));
   }, []);
 
-  return <div></div>;
+  return (
+    <div className="my-10">
+      <div className="py-8 text-3xl text-center">
+        <Title text1={'BEST'} text2={'SELLERS'} />
+        <p></p>
+      </div>
+    </div>
+  );
 };
 
 export default BestSeller;
