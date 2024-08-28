b75580d2c8a216bbce3a8fb5fd787ed0969bc7aa
ghmamadreza2 <ghmamadreza84@gmail.com>
2024-08-28 23:44:08 +0430

    i add cagetory section and start develope here

--- src/pages/Collection.jsx
+++ src/pages/Collection.jsx
@@ -6,7 +6,11 @@ const Collection = () => {
     <div className="flex flex-col gap-1 pt-10 border-t sm:flex-row sm:gap-10 ">
       {/* Filter Options  */}
       <div className="min-w-60">
-        <p className="flex items-center gap-2 my-2 text-xl cursor-pointer">FILTERS</p>
+        <p className="flex items-center gap-2 my-2 text-xl cursor-pointer">
+          FILTERS
+        </p>
+        {/* Category Filter */}
+        <div className={`border border-gray-300 pl-5 py-3 mt-6`}></div>
       </div>
     </div>
   );
