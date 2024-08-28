88999b3b2771ded428c89bdb21a49ce941e65797
ghmamadreza2 <ghmamadreza84@gmail.com>
2024-08-28 23:33:43 +0430

    i first go add a filter option section for collection page

--- src/pages/Collection.jsx
+++ src/pages/Collection.jsx
@@ -3,7 +3,10 @@ import ShopContext from "../context/ShopContext";
 const Collection = () => {
   const { products } = useContext(ShopContext);
   return (
-    <div className="flex flex-col gap-1 pt-10 border-t sm:flex-row sm:gap-10 "></div>
+    <div className="flex flex-col gap-1 pt-10 border-t sm:flex-row sm:gap-10 ">
+      {/* Filter Options  */}
+      <div className=""></div>
+    </div>
   );
 };
 
