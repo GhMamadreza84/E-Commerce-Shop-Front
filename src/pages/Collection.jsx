c5a1c71d893eb3bc7ad53a30352f539506c8c1e6
ghmamadreza2 <ghmamadreza84@gmail.com>
2024-08-28 23:41:12 +0430

    i add a filter title in collection page component

--- src/pages/Collection.jsx
+++ src/pages/Collection.jsx
@@ -5,7 +5,9 @@ const Collection = () => {
   return (
     <div className="flex flex-col gap-1 pt-10 border-t sm:flex-row sm:gap-10 ">
       {/* Filter Options  */}
-      <div className=""></div>
+      <div className="min-w-60">
+        <p className="flex items-center gap-2 my-2 text-xl cursor-pointer">FILTERS</p>
+      </div>
     </div>
   );
 };
