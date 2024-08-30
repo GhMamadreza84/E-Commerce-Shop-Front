7eb3618480154f4d7057507edf8f6f650f1570d5
ghmamadreza2 <ghmamadreza84@gmail.com>
2024-08-30 20:33:28 +0430

    i add togglecategory function to category inputs and i now go test this logic

--- src/pages/Collection.jsx
+++ src/pages/Collection.jsx
@@ -47,13 +47,13 @@ const Collection = () => {
           <p className="mb-3 text-sm font-medium ">CATEGORIES</p>
           <div className="flex flex-col gap-2 text-sm font-light text-gray-700">
             <p className="flex gap-2">
-              <input className="w-3 " type="checkbox" value={"Men"} /> Men
+              <input className="w-3 " type="checkbox" value={"Men"} onChange={toggleCategory}/> Men
             </p>
             <p className="flex gap-2">
-              <input className="w-3 " type="checkbox" value={"Women"} /> Women
+              <input className="w-3 " type="checkbox" value={"Women"} onChange={toggleCategory}/> Women
             </p>
             <p className="flex gap-2">
-              <input className="w-3 " type="checkbox" value={"Kids"} /> Kids
+              <input className="w-3 " type="checkbox" value={"Kids"} onChange={toggleCategory}/> Kids
             </p>
           </div>
         </div>
