3569888b843cb5313f3076713187a8e309e32cd7
ghmamadreza2 <ghmamadreza84@gmail.com>
2024-08-30 20:28:53 +0430

    i add new logic to collection  name function is togglecategory for filter category on products

--- src/pages/Collection.jsx
+++ src/pages/Collection.jsx
@@ -10,6 +10,15 @@ const Collection = () => {
   const [filterProducts, setFilterProducts] = useState([]);
   const [category, setCategory] = useState([]);
   const [subCategory, setSubCategory] = useState([]);
+
+  const toggleCategory = (e) => {
+    if (category.includes(e.target.value)) {
+      setCategory((prev) => prev.filter((item) => item !== e.target.value));
+    } else {
+      setCategory((prev) => [...prev, e.target.value]);
+    }
+  };
+
   useEffect(() => {
     setFilterProducts(products);
   }, []);
