d018a21dcbed54d3c9a4abffd56f32cd99d025d5
ghmamadreza2 <ghmamadreza84@gmail.com>
2024-08-30 23:21:06 +0430

    i create new logic for filter products with sort

--- src/pages/Collection.jsx
+++ src/pages/Collection.jsx
@@ -46,6 +46,23 @@ const Collection = () => {
     setFilterProducts(productCopy);
   };
 
+  const sortProducts = () => {
+    let fpCopy = filterProducts.slice();
+    switch (sortType) {
+      case "low-high":
+        setFilterProducts(fpCopy.sort(a, (b) => a.price - b.price));
+        break;
+
+      case "high-low":
+        setFilterProducts(fpCopy.sort(a, (b) => b.price - a.price));
+        break;
+
+      default:
+        applyFilter();
+        break;
+    }
+  };
+
   useEffect(() => {
     applyFilter();
   }, [category, subCategory]);
