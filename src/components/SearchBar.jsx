91cbb63198b4d5355e2db12bc11c3e50afd44ff8
ghmamadreza2 <ghmamadreza84@gmail.com>
2024-08-31 23:27:37 +0430

    i create input tag and add styles in search bar component now i need complate this component and write logic for when show and when hide

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -4,7 +4,19 @@ import { ShopContext } from "../context/ShopContext";
 const SearchBar = () => {
   const { search, setSearch, showSearch, setShowSearch } =
     useContext(ShopContext);
-  return showSearch ? <div></div> : null;
+  return showSearch ? (
+    <div className="text-center border-t border-b bg-gray-50">
+      <div className="inline-flex items-center justify-center w-3/4 px-5 py-2 mx-3 my-5 border border-gray-400 rounded-full sm:w-1/2">
+        <input
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+          className="flex-1 text-sm outline-none bg-inherit"
+          type="text"
+          placeholder="Search"
+        />
+      </div>
+    </div>
+  ) : null;
 };
 
 export default SearchBar;
