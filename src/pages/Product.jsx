07b02e031c7fe8d31b6320d95a240a6621580d9d
ghmamadreza2 <ghmamadreza84@gmail.com>
2024-09-03 07:56:18 +0430

    i get product id with hook use params in product page

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,11 +1,10 @@
-import React from 'react'
+import React from "react";
+import { useParams } from "react-router-dom";
 
 const Product = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const { productId } = useParams();
+  console.log(productId)
+  return <div></div>;
+};
 
-export default Product
+export default Product;
