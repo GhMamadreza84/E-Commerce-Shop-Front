2dc915af0ea51a1f09231d5e4dafe9a63bdf8312
ghmamadreza2 <ghmamadreza84@gmail.com>
2024-09-03 08:03:55 +0430

    i create use effect in product page

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { ShopContext } from "../context/ShopContext";
 
@@ -8,6 +8,10 @@ const Product = () => {
   const { productData, setProductData } = useState(false);
 
   const fetchProductData = async () => {};
+
+  useEffect(() => {
+    fetchProductData();
+  }, [productId, products]);
 
   return <div></div>;
 };
