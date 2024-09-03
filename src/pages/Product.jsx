2020311bb7ecf67436f2132323bc7389ef39a03d
ghmamadreza2 <ghmamadreza84@gmail.com>
2024-09-03 08:21:27 +0430

    i map on the images of product page and now show it in ui

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -29,7 +29,11 @@ const Product = () => {
         {/* Product Images */}
         <div className="flex flex-col-reverse flex-1 gap-3 sm:flex-row ">
           <div className="flex flex-col justify-between overflow-x-auto sm:overflow-y-auto sm:justify-normal sm:w-[18.7%] w-full ">
-            
+            {
+              productData.image.map((item,index)=>(
+                <img className="w-[24%] sm:w-full sm:mb-3 flex-shrink-0 cursor-pointer " src={item } key={index} alt='image-product' />
+              ))
+            }
           </div>
         </div>
       </div>
