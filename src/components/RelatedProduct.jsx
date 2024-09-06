04869727b843c87cf03cc3280eb52e83d96f9a55
ghmamadreza2 <ghmamadreza84@gmail.com>
2024-09-06 23:40:28 +0430

    i complate related product section

--- src/components/RelatedProduct.jsx
+++ src/components/RelatedProduct.jsx
@@ -1,6 +1,7 @@
 import React, { useContext, useEffect, useState } from "react";
 import { ShopContext } from "../context/ShopContext";
-
+import Title from "./Title";
+import ProductItem from "../components/ProductItem";
 const RelatedProduct = ({ category, subCategory }) => {
   const { products } = useContext(ShopContext);
   const [related, setRelated] = useState([]);
@@ -12,10 +13,27 @@ const RelatedProduct = ({ category, subCategory }) => {
       productCopy = productCopy.filter(
         (item) => subCategory === item.subCategory
       );
-      setRelated(productCopy.slice(0, 5));
+      setRelated(productCopy.slice(1, 6));
     }
   }, [products]);
-  return <div></div>;
+  return (
+    <div className="my-24">
+      <div className="py-2 text-3xl text-center ">
+        <Title text1={"RELATED"} text2={"PRODUCT"} />
+        <div className="grid grid-cols-2 gap-4 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-y-6">
+          {related.map((item, index) => (
+            <ProductItem
+              key={index}
+              id={item._id}
+              name={item.name}
+              image={item.image}
+              price={item.price}
+            />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default RelatedProduct;
