e6f83009c112be57f446195fe40f7030ac098d5c
ghmamadreza2 <ghmamadreza84@gmail.com>
2024-09-06 23:23:12 +0430

    i filter product with category for show same category product

--- src/components/RelatedProduct.jsx
+++ src/components/RelatedProduct.jsx
@@ -1,9 +1,16 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { ShopContext } from "../context/ShopContext";
 
 const RelatedProduct = ({ category, subCategory }) => {
   const { products } = useContext(ShopContext);
   const [related, setRelated] = useState([]);
+  console.log(category)
+  useEffect(() => {
+    if (products.length > 0) {
+      let productCopy = products.slice();
+      productCopy = productCopy.filter((item) => category === item.category);
+    }
+  }, []);
   return <div></div>;
 };
 
