258c2f52fe06da9c7f8c4ee6e84d0e02f9ba8412
ghmamadreza2 <ghmamadreza84@gmail.com>
2024-09-07 23:29:15 +0430

    i write a function for when one product we have in cart add one more to this

--- src/context/ShopContext.jsx
+++ src/context/ShopContext.jsx
@@ -9,6 +9,16 @@ const ShopContextProvider = (props) => {
   const [search, setSearch] = useState("");
   const [showSearch, setShowSearch] = useState(false);
   const [cartItems, setCartItems] = useState({});
+
+  const addToCart = async (itemId, size) => {
+    let cartData = structuredClone(cartItems);
+    if (cartData[itemId]) {
+      if (cartData[itemId][size]) {
+        cartData[itemId][size] += 1;
+      }
+    }
+  };
+
   const value = {
     products,
     currency,
