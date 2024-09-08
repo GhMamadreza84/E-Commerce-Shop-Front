d9850a1617fa982bd9c11aac8982438e8b4a49bb
ghmamadreza2 <ghmamadreza84@gmail.com>
2024-09-08 19:34:09 +0430

    i now get data from cartItems and push it in tempData

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,10 +1,26 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { ShopContext } from "../context/ShopContext";
 
 const Cart = () => {
   const { products, currency, cartItems } = useContext(ShopContext);
 
   const [cartData, setCartData] = useState([]);
+
+  useEffect(() => {
+    let tempData = [];
+    for (const items in cartItems) {
+      for (const item in cartItems[items]) {
+        if (cartItems[items][item] > 0) {
+          tempData.push({
+            _id: items,
+            size: item,
+            quantity: cartItems[items][item],
+          });
+        }
+      }
+    }
+    console.log(tempData)
+  }, [cartItems]);
 
   return <div></div>;
 };
