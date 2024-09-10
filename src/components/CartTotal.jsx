e6a260a1406230f65e740e78e63bd8a10e6fb759
ghmamadreza2 <ghmamadreza84@gmail.com>
2024-09-10 20:34:12 +0430

    i get the wrong function from shop context in carttotal component i edit it

--- src/components/CartTotal.jsx
+++ src/components/CartTotal.jsx
@@ -1,12 +1,23 @@
 import React, { useContext } from "react";
 import { ShopContext } from "../context/ShopContext";
+import Title from "./Title";
+import { getSearchParamsForLocation } from "react-router-dom/dist/dom";
 
 const CartTotal = () => {
-  const { currency, getTotalAmount, delivery_fee } = useContext(ShopContext);
+  const { currency, getCartAmount, delivery_fee } = useContext(ShopContext);
 
-  return <div className="w-full">
-     
-  </div>;
+  return (
+    <div className="w-full">
+      <div className="text-2xl">
+        <Title text1={"CART"} text2={"TOTALS"} />
+      </div>
+      <div className="flex flex-col gap-2 mt-2 text-sm">
+        <div className="flex justify-between ">
+          {currency} {getSearchParamsForLocation}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default CartTotal;
