9e26416855e43f21b28846b29f49997f4366c632
ghmamadreza2 <ghmamadreza84@gmail.com>
2024-09-10 20:38:35 +0430

    i create section for show cartTotal component in cart

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -2,6 +2,7 @@ import React, { useContext, useEffect, useState } from "react";
 import { ShopContext } from "../context/ShopContext";
 import Title from "../components/Title";
 import { assets } from "../assets/assets";
+import CartTotal from "../components/CartTotal";
 const Cart = () => {
   const { products, currency, cartItems, updateQuantity } =
     useContext(ShopContext);
@@ -84,6 +85,11 @@ const Cart = () => {
             </div>
           );
         })}
+      </div>
+      <div className="flex justify-end my-20">
+        <div className="wfull sm:w-[450px]">
+            <CartTotal />
+        </div>
       </div>
     </div>
   );
