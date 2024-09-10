6773dd1018786f01f1415687fcbeef81e8d0ab57
ghmamadreza2 <ghmamadreza84@gmail.com>
2024-09-10 20:31:21 +0430

    i forgot get delivery_fee in cartTotal component i now get it from useContext

--- src/components/CartTotal.jsx
+++ src/components/CartTotal.jsx
@@ -2,7 +2,7 @@ import React, { useContext } from "react";
 import { ShopContext } from "../context/ShopContext";
 
 const CartTotal = () => {
-  const { currency, getTotalAmount } = useContext(ShopContext);
+  const { currency, getTotalAmount, delivery_fee } = useContext(ShopContext);
 
   return <div></div>;
 };
