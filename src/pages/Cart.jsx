39fb5e961a71f9d81dfb0fe7728fdf11fb158757
ghmamadreza2 <ghmamadreza84@gmail.com>
2024-09-10 20:58:40 +0430

    i add a navigate hook to prooceed to check out button for user after click on the button go to place-order page

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -4,7 +4,7 @@ import Title from "../components/Title";
 import { assets } from "../assets/assets";
 import CartTotal from "../components/CartTotal";
 const Cart = () => {
-  const { products, currency, cartItems, updateQuantity } =
+  const { products, currency, cartItems, updateQuantity, navigate } =
     useContext(ShopContext);
 
   const [cartData, setCartData] = useState([]);
@@ -88,10 +88,15 @@ const Cart = () => {
       </div>
       <div className="flex justify-end my-20">
         <div className="w-full sm:w-[450px]">
-        <CartTotal />
-        <div className="w-full text-end">
-          <button className="px-8 py-3 my-8 text-sm text-white bg-black">PROCCED TO CHECKOUT</button>
-        </div>
+          <CartTotal />
+          <div className="w-full text-end">
+            <button
+              onClick={() => navigate("/place-order")}
+              className="px-8 py-3 my-8 text-sm text-white bg-black"
+            >
+              PROCCED TO CHECKOUT
+            </button>
+          </div>
         </div>
       </div>
     </div>
