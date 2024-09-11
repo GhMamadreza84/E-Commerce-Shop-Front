6915cbbe1242db28bed8d2aca082b42cc7bbff35
ghmamadreza2 <ghmamadreza84@gmail.com>
2024-09-11 20:24:01 +0430

    i import the title component in place order page and use it

--- src/pages/PlaceOrder.jsx
+++ src/pages/PlaceOrder.jsx
@@ -1,11 +1,17 @@
-import React from 'react'
+import React from "react";
+import Title from "../components/Title";
 
 const PlaceOrder = () => {
   return (
-    <div className='flex flex-col justify-between gap-4 sm:flex-row pt-5 sm:pt-14 min-h-[80vh] border-t '>
-      <div className=""></div>
+    <div className="flex flex-col justify-between gap-4 sm:flex-row pt-5 sm:pt-14 min-h-[80vh] border-t ">
+      {/* --------------Left  Side---------------- */}
+      <div className="flex flex-col w-full gap-4 sm:max-w-[480px]">
+        <div className="my-3 text-xl sm:text-2xl">
+          <Title text1={"DELIVERY"} text2={"INFORMATION"} />
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default PlaceOrder
+export default PlaceOrder;
