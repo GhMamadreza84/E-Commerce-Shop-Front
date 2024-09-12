00862754c90a11ef3a81f0d3f06742548d7933ca
ghmamadreza2 <ghmamadreza84@gmail.com>
2024-09-12 10:11:53 +0430

    i import the title component and use it in orders page

--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -1,9 +1,16 @@
 import React, { useContext } from "react";
 import { ShopContext } from "../context/ShopContext";
+import Title from "../components/Title";
 
 const Orders = () => {
-  const { products } = useContext(ShopContext);
-  return <div></div>;
+  const { products, currency } = useContext(ShopContext);
+  return (
+    <div className="pt-16 border-t">
+      <div className="text-2xl ">
+        <Title text1={'MY'} text2={'ORDERS'} />
+      </div>
+    </div>
+  );
 };
 
 export default Orders;
