ed4cdd54a7fb8ac61d607f832e82f04b01fa8cfc
ghmamadreza2 <ghmamadreza84@gmail.com>
2024-09-13 18:41:56 +0430

    i add a input filds for login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -4,7 +4,30 @@ const Login = () => {
   const [currentState, setCurrentState] = useState("Sign Up");
 
   return (
-    <form className="flex flex-col items-center w-[90%] sm:w-96 mt-14 m-auto gap-4 text-gray-800"></form>
+    <form className="flex flex-col items-center w-[90%] sm:w-96 mt-14 m-auto gap-4 text-gray-800">
+      <div className="inline-flex items-center gap-2 mt-10 mb-2 ">
+        <p className="text-3xl prata-regular ">{currentState}</p>
+        <hr className="border-none h-[1.5px] w-8 bg-gray-800" />
+      </div>
+      <input
+        type="text"
+        className="w-full px-3 py-2 border border-gray-800 "
+        placeholder="Name"
+        required
+      />
+      <input
+        type="email"
+        className="w-full px-3 py-2 border border-gray-800 "
+        placeholder="Email"
+        required
+      />
+      <input
+        type="password"
+        className="w-full px-3 py-2 border border-gray-800 "
+        placeholder="Password"
+        required
+      />
+    </form>
   );
 };
 
