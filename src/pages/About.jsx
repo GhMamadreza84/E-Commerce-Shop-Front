099d33622a774a34b64bf7bd1755d6d1edf8f612
ghmamadreza2 <ghmamadreza84@gmail.com>
2024-09-13 19:05:04 +0430

    i import title component in about page and use it

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,11 +1,12 @@
-import React from 'react'
-
+import React from "react";
+import Title from "../components/Title";
 const About = () => {
   return (
-    <div className='text-2xl text-center'>
+    <div className="pt-8 text-2xl text-center border-t">
+      <Title text1={"ABOUT"} text2={"US"} />
       
     </div>
-  )
-}
+  );
+};
 
-export default About
+export default About;
