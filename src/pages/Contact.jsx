27562dbe7a6aab5347ee3a0ed871bb50b2499f94
ghmamadreza2 <ghmamadreza84@gmail.com>
2024-09-13 19:26:11 +0430

    i start create contact us page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import Title from '../components/Title'
 
 const Contact = () => {
   return (
     <div>
-      
+      <div className="pt-10 text-2xl text-center border-t">
+        <Title text1={'CONTACT'} text2={'US'} />
+      </div>
     </div>
   )
 }
