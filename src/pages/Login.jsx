b2d3ec46e99d9ef714071041e1d5bf6821fda437
ghmamadreza2 <ghmamadreza84@gmail.com>
2024-09-13 18:36:56 +0430

    i create form tag in login and add a classname property to this

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -3,7 +3,9 @@ import React, { useState } from "react";
 const Login = () => {
   const [currentState, setCurrentState] = useState("Sign Up");
 
-  return <div></div>;
+  return (
+    <form className="flex flex-col items-center w-[90%] sm:w-96 mt-14 m-auto gap-4 text-gray-800"></form>
+  );
 };
 
 export default Login;
